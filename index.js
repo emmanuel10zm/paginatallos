3bf52c7aa780ddb18b004d25d04a2f28a9ee7829
emmanuel10zm <95731117+emmanuel10zm@users.noreply.github.com>
2022-07-01 15:39:05 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const ntoggle = document.querySelector(".ntoggle")
+const navmenu = document.querySelector(".menuul")
+const navlist = document.querySelector(".menuitem")
+const navlist2 = document.querySelector(".menuitem2")
+const navlist3 = document.querySelector(".menuitem3")
+const navlist4 = document.querySelector(".menuitem4")
+const navlist5 = document.querySelector(".menuitem5")
+
+ntoggle.addEventListener("click", () => {
+    navmenu.classList.toggle("menuul_visible");
+});
+navlist.addEventListener("click", () => {
+    navmenu.classList.toggle("menuul_visible");
+});
+navlist2.addEventListener("click", () => {
+    navmenu.classList.toggle("menuul_visible");
+});
+navlist3.addEventListener("click", () => {
+    navmenu.classList.toggle("menuul_visible");
+});
+navlist4.addEventListener("click", () => {
+    navmenu.classList.toggle("menuul_visible");
+});
+navlist5.addEventListener("click", () => {
+    navmenu.classList.toggle("menuul_visible");
+});
